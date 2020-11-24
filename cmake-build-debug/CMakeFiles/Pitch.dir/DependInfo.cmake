5f99e00bdae8634bda387d8b3cf6b0232f7ce2a2
xiaofei-dev <xiaofei.dev@gmail.com>
2020-11-24 11:33:21 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/Pitch.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/CPP/Pitch/main.cpp" "D:/CPP/Pitch/cmake-build-debug/CMakeFiles/Pitch.dir/main.cpp.obj"
+  "D:/CPP/Pitch/pitch.cpp" "D:/CPP/Pitch/cmake-build-debug/CMakeFiles/Pitch.dir/pitch.cpp.obj"
+  "D:/CPP/Pitch/pitch_op.cpp" "D:/CPP/Pitch/cmake-build-debug/CMakeFiles/Pitch.dir/pitch_op.cpp.obj"
+  "D:/CPP/Pitch/resample.cpp" "D:/CPP/Pitch/cmake-build-debug/CMakeFiles/Pitch.dir/resample.cpp.obj"
+  "D:/CPP/Pitch/support_functions.cpp" "D:/CPP/Pitch/cmake-build-debug/CMakeFiles/Pitch.dir/support_functions.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Pitch.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pitch.dir/main.cpp.obj"
+  "CMakeFiles/Pitch.dir/pitch.cpp.obj"
+  "CMakeFiles/Pitch.dir/pitch_op.cpp.obj"
+  "CMakeFiles/Pitch.dir/resample.cpp.obj"
+  "CMakeFiles/Pitch.dir/support_functions.cpp.obj"
+  "Pitch.exe"
+  "Pitch.exe.manifest"
+  "Pitch.pdb"
+  "libPitch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pitch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
